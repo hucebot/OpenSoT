b731298abef856b632f066d21994ee6c800243c8
EnricoMingo <enricomingo@gmail.com>
2026-03-12 11:31:03 +0100

    Added more generic methods to model Omni/Mechanum-Wheels and bases using these wheels. Added tests

--- tests/utils/cartesian_utils_test.cpp
+++ tests/utils/cartesian_utils_test.cpp
@@ -77,6 +77,47 @@ TEST_F(testCartesianUtils, testPseudoInverse1)
 }
 
 
+TEST_F(testCartesianUtils, testMechanum4XJacobian)
+{
+    double px = 0.5;
+    double py = 0.3;
+    double r = 0.025;
+
+    Eigen::MatrixXd J = cartesian_utils::Mechanum4XJacobian(px, py, r);
+    std::cout<<"Mechanum4XJacobian: \n"<<J<<std::endl;
+
+    Eigen::MatrixXd H(4,3);
+    H.row(0) = Eigen::Vector3d(1, -1, -px -py);
+    H.row(1) = Eigen::Vector3d(1,  1,  px +py);
+    H.row(2) = Eigen::Vector3d(1,  1, -px -py);
+    H.row(3) = Eigen::Vector3d(1, -1,  px +py);
+    H *= (1./r);
+    std::cout<<"H: \n"<<H<<std::endl;
+
+    for(unsigned int i = 0; i < J.rows(); ++i)
+    {
+        for(unsigned int j = 0; j < J.cols(); ++j)
+            EXPECT_DOUBLE_EQ(J(i,j), H(i,j));
+    }
+
+    Eigen::MatrixXd Ji = (J.transpose() * J).inverse() * J.transpose();
+    std::cout<<"Mechanum4XJacobian Inverse: \n"<<Ji<<std::endl;
+
+    Eigen::MatrixXd Hi(3,4);
+    Hi.col(0) = Eigen::Vector3d(1, -1, -1./(px + py));
+    Hi.col(1) = Eigen::Vector3d(1,  1,  1./(px + py));
+    Hi.col(2) = Eigen::Vector3d(1,  1, -1./(px + py));
+    Hi.col(3) = Eigen::Vector3d(1, -1,  1./(px + py));
+    Hi *= (r/4.);
+    std::cout<<"Hi: \n"<<Hi<<std::endl;
+
+    for(unsigned int i = 0; i < Ji.rows(); ++i)
+    {
+        for(unsigned int j = 0; j < Ji.cols(); ++j)
+            EXPECT_DOUBLE_EQ(Ji(i,j), Hi(i,j));
+    }
+}
+
 
 
 
