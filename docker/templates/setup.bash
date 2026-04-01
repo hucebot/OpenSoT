6c33046f74f8747774953b43cc4b98f7afed9a0f
Olivier Rochel <olivier.rochel@inria.fr>
2026-04-01 15:53:08 +0200

    ci: add docker stuff from inria docker for testing

---
+++ docker/templates/setup.bash
@@ -0,0 +1,9 @@
+export LD_LIBRARY_PATH=/home/forest_ws/install/lib:$LD_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=/home/forest_ws/install:$CMAKE_PREFIX_PATH
+export PATH=/home/forest_ws/install/bin:$PATH
+export GAZEBO_PLUGIN_PATH=/home/forest_ws/install/lib:$GAZEBO_PLUGIN_PATH
+export PYTHONPATH=/home/forest_ws/install/lib/python3.12/site-packages:/home/forest_ws/install/lib/python3/dist-packages:$PYTHONPATH
+#export ROS_PACKAGE_PATH=/home/forest_ws/ros_src:/home/forest_ws/install/share:/home/forest_ws/install/lib:$ROS_PACKAGE_PATH
+#export AMENT_PREFIX_PATH=/home/forest_ws/install:$AMENT_PREFIX_PATH
+export PKG_CONFIG_PATH=/home/forest_ws/install/lib/pkgconfig:$PKG_CONFIG_PATH
+# export HHCM_FOREST_PATH=/home/forest_ws:$HHCM_FOREST_PATH
