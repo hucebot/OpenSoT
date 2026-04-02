ceb79817ff212db7250b2bbbef01644a79ec732d
Olivier Rochel <olivier.rochel@inria.fr>
2026-04-02 14:34:18 +0200

    ci: cleanup templates/ again

--- docker/templates/setup.bash
+++ docker/templates/setup.bash
@@ -1,7 +1,6 @@
 export LD_LIBRARY_PATH=/home/forest_ws/install/lib:$LD_LIBRARY_PATH
 export CMAKE_PREFIX_PATH=/home/forest_ws/install:$CMAKE_PREFIX_PATH
 export PATH=/home/forest_ws/install/bin:$PATH
-export GAZEBO_PLUGIN_PATH=/home/forest_ws/install/lib:$GAZEBO_PLUGIN_PATH
 export PYTHONPATH=/home/forest_ws/install/lib/python3.12/site-packages:/home/forest_ws/install/lib/python3/dist-packages:$PYTHONPATH
 export PKG_CONFIG_PATH=/home/forest_ws/install/lib/pkgconfig:$PKG_CONFIG_PATH
 
